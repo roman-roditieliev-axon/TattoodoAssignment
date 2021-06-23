c48112e8b53d6a6d5a8de8b6de88101bec489199
roman-roditieliev-axon <rroditieliev@axon.dev>
2021-06-23 13:34:50 +0300

    feat: TEST-1 - create new project

---
+++ Shared/TattoodoAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TattoodoAssignmentApp.swift
+//  Shared
+//
+//  Created by User on 23.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TattoodoAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
