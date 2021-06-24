72514f2c67a167ffc800956401a697c4b1b7c974
roman-roditieliev-axon <rroditieliev@axon.dev>
2021-06-24 07:41:30 +0300

    feat: TEST-1 - add AppDelegate and folders

--- Shared/TattoodoAssignmentApp.swift
+++ Shared/TattoodoAssignmentApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct TattoodoAssignmentApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
